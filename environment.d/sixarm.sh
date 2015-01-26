aaa06dfef8fcd38f4a25456ed1ecc1b75c164884
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-01-25 19:42:45 -0700

    Add new SixArm script repos

--- environment.d/sixarm.sh
+++ environment.d/sixarm.sh
@@ -1,6 +1,7 @@
 # SixArm.com repositories
 
-OPT=$OPT:/opt/sixarm_debian_dpkg_scripts
+OPT=$OPT:/opt/sixarm/sixarm_apt_scripts
+OPT=$OPT:/opt/sixarm/sixarm_debian_dpkg_scripts
 OPT=$OPT:/opt/sixarm/sixarm_gem_scripts
 OPT=$OPT:/opt/sixarm/sixarm_git_gitconfig
 OPT=$OPT:/opt/sixarm/sixarm_htx
@@ -8,5 +9,8 @@ OPT=$OPT:/opt/sixarm/sixarm_mysql_scripts
 OPT=$OPT:/opt/sixarm/sixarm_postgresql_scripts
 OPT=$OPT:/opt/sixarm/sixarm_ruby_gem_scripts
 OPT=$OPT:/opt/sixarm/sixarm_ruby_gemforge
+OPT=$OPT:/opt/sixarm/sixarm_shell_random
+OPT=$OPT:/opt/sixarm/sixarm_shell_statistics
+OPT=$OPT:/opt/sixarm/sixarm_ssh_scripts
 OPT=$OPT:/opt/sixarm/sixarm_strategem
 OPT=$OPT:/opt/sixarm/sixarm_unix_shell_scripts
