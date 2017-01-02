fb101f6cd1395147ac9a32f21fa74423d50f1474
Joel Parker Henderson <joel@joelparkerhenderson.com>
2017-01-02 13:02:32 -0800

    Add lapack

---
+++ environment.d/lapack.sh
@@ -0,0 +1,26 @@
+# lapack
+#
+# If you use macOS and use brew, then your lapack
+# is likely to be installed in a way that needs these:
+#
+#    LDFLAGS: -L/usr/local/opt/lapack/lib
+#    CPPFLAGS: -I/usr/local/opt/lapack/include
+#    PKG_CONFIG_PATH: /usr/local/opt/lapack/lib/pkgconfig
+#
+# If you use cross-platform lapack and cross-platform /etc/environment,
+# such as macOS with brew, and Linux, and this specific environment file,
+# then you will likely want to normalize the installation locations;
+# link the macOS location to our naming-convention location like this:
+#
+#     mkdir /opt/lapack
+#     ln -s /usr/local/opt/lapack /opt/lapack/current
+
+OPT="$OPT:/opt/lapack/current/bin"
+
+LAPACK_CPPFLAGS="-I/usr/local/opt/lapack/include"
+LAPACK_LDFLAGS="-L/opt/lapack/current/lib"
+LAPACK_PKG_CONFIG_PATH="/usr/local/opt/lapack/lib/pkgconfig"
+
+CPPFLAGS="$CPPFLAGS $LAPACK_CPPFLAGS"
+LDFLAGS="$LDFLAGS $LAPACK_LDFLAGS"
+PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$LAPACK_PKG_CONFIG_PATH"
