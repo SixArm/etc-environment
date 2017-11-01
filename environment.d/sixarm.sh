af86758801c862f630318e450139a4256da8a6b6
Joel Parker Henderson <joel@joelparkerhenderson.com>
2017-11-01 21:26:07 +0700

    Add scripts

--- environment.d/sixarm.sh
+++ environment.d/sixarm.sh
@@ -1,10 +1,12 @@
 # SixArm.com repositories
 
+OPT=$OPT:/opt/sixarm/airport-ssid
 OPT=$OPT:/opt/sixarm/gitconfig
 OPT=$OPT:/opt/sixarm/gpg-decrypt
 OPT=$OPT:/opt/sixarm/gpg-encrypt
 OPT=$OPT:/opt/sixarm/log-file-cleaning
 OPT=$OPT:/opt/sixarm/rsync-mirror
+OPT=$OPT:/opt/sixarm/ssid-powerful
 OPT=$OPT:/opt/sixarm/sixarm_apt_scripts
 OPT=$OPT:/opt/sixarm/sixarm_dpkg_builder
 OPT=$OPT:/opt/sixarm/sixarm_dpkg_scripts
@@ -22,4 +24,3 @@ OPT=$OPT:/opt/sixarm/sixarm_shell_statistics
 OPT=$OPT:/opt/sixarm/sixarm_ssh_scripts
 OPT=$OPT:/opt/sixarm/sixarm_strategem
 OPT=$OPT:/opt/sixarm/sixarm_unix_shell_scripts
-
