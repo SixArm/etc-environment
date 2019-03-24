83ecb518f66c47c807d2b39a83368baf8b0be0e3
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-03-24 14:12:12 -0700

    Add Android NPK

--- environment.d/android.sh
+++ environment.d/android.sh
@@ -1,4 +1,6 @@
 # Android
 
-ANDROID_HOME=/opt/android-sdk
+ANDROID_HOME="/opt/android-sdk"
 PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
+
+ANDROID_NDK_HOME="/usr/local/share/android-ndk"
