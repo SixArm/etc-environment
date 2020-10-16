a03d189e79dfa789f27c62c0b27845d0f6b1c9b0
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-10-15 21:00:10 -0700

    Add Postgres for macOS via EDB installer

--- environment.d/postgres.app.sh
+++
@@ -1,3 +0,0 @@
-# Postgres.app database on OSX
-
-PATH="$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin"
--- environment.d/postgres.sh
+++ environment.d/postgres.sh
@@ -1,4 +1,9 @@
-# Postgres database
-
+# Postgres database via typical Linux installer
 PATH="$PATH:/usr/local/pgsql/bin"
 MANPATH="$MANPATH:/usr/local/pgsql/man"
+
+# Postgres database for macOS via EDB installer
+PATH="$PATH:/Library/PostgreSQL/13/bin"
+
+# Postgres daabase for macOS via Postgres.app application
+PATH="$PATH:/Applications/Postgres.app/Contents/Versions/13.0/bin"
